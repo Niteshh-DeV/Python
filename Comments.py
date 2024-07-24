738d8aae38a864ab96f80feb0eef56c9049e548b
Nitesh Joshi <niteshjo.67894@gmail.com>
2024-07-24 14:54:34 +0545

    Dammar

---
+++ Comments.py
@@ -0,0 +1,8 @@
+#This is Single line Comment
+"""
+This is Multi line comment.
+In this We can Comment On Multi lines
+"""
+
+print("Comment Shortcut is Ctrl+ /")
+print("For single line Comment # sign is used But \n For Multiline Comment \"\"\" This is Comment \"\"\" is used")
