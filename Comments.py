5d9e5042de055a5c98b5f7db09dd41ec2fe14616
Nitesh Joshi <niteshjo.67894@gmail.com>
2024-09-10 23:02:18 +0545

    Delete Comments.py

--- Comments.py
+++
@@ -1,8 +0,0 @@
-#This is Single line Comment
-"""
-This is Multi line comment.
-In this We can Comment On Multi lines
-"""
-
-print("Comment Shortcut is Ctrl+ /")
-print("For single line Comment # sign is used But \n For Multiline Comment \"\"\" This is Comment \"\"\" is used")
