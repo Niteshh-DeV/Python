79078414d6efce8f9f2a187586611e22c253f9aa
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-02-16 23:33:21 +0545

    Converting List to NumPy array

---
+++ NumPy/ListToNumpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+list1 = [12,23,13,10,20]
+
+np_array = np.asarray(list1) # Convert list to numpy array
+print(np_array)
+print(type(np_array))   # ndarray is the data type of the array in numpy. Its N dimensional array.
