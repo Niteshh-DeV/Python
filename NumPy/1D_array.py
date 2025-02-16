9ac9126d805204bff746d788115f77a6cf0dba14
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-02-16 23:29:51 +0545

    1D array in numpy and its indexing

---
+++ NumPy/1D_array.py
@@ -0,0 +1,11 @@
+import numpy as np 
+
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+
+print(a.shape)  # (5,) means 1D array with 5 elements in it. It gives the shape of the array. Rows, columns, etc.
+print(a[0])     # 1
+print(a[1])     # 2
+print(a[2])     # 3
+print(a[3])     # 4
+print(a[4])     # 5
