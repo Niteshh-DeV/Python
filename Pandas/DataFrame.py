70ea3394f9dc81f7d032b64fb72a2343648b05d4
Nitesh Joshi <niteshjoshi010@gmail.com>
2025-03-01 10:43:10 +0545

    Importing Datasets

---
+++ Pandas/DataFrame.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#importing the dataset
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+# print (type(iris))
+
+# print(iris)
+
+from sklearn.datasets import load_diabetes
+diabetes = load_diabetes()
+print(diabetes)
